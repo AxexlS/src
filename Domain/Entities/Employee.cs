09278602d4e439b5f510aeadf7de4c8f97fb60b2
Osss77 <58944809+Osss77@users.noreply.github.com>
2019-12-20 16:53:49 +0200

    Add data annotations

--- Domain/Entities/Employee.cs
+++ Domain/Entities/Employee.cs
@@ -7,25 +7,33 @@ namespace OS.WpfDevExpressPlc.Domain.Entities
     public class Employee
     {
         [Name("FirstName")]
-        [ReadOnly(true)]
-        [StringLengthAttribute(50, ErrorMessage = "Please input name")]
-        //[RequiredAttribute(false, ErrorMessage = "Please input name")]
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
         public string FirstName { get; set; }
 
         [Name("LastName")]
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
         public string LastName { get; set; }
 
         [Name("Position")]
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
         public string Position { get; set; }
 
         [Name("Office")]
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
         public string Office { get; set; }
 
         [Name("StartDate")]
-
+        [Required]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
         public string StartDate { get; set; }
 
         [Name("Salary")]
+        [Required]
+        [Range(1, 100000000)]
         public string Salary { get; set; }
 
     }
